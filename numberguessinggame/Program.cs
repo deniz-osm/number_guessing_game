d9846763e929a9f212996b868d89d9e59673dc29
denizaosm <151645611+deniz-osm@users.noreply.github.com>
2024-04-20 14:14:43 +0300

    Add files via upload

---
+++ numberguessinggame/Program.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        bool isCorrectGuess = false;
+        Random random = new Random();
+
+        int randomNum = random.Next(1, 11);
+        Console.WriteLine("Welcome to the number guessing game.");
+        Console.WriteLine("Number between 1 and 10 will be generated.");
+        Console.WriteLine("If you guess the correct number, you win!");
+
+        Console.WriteLine(randomNum);
+
+        while (!isCorrectGuess)
+        {
+            Console.WriteLine("Please enter your guess.");
+            int guess = Convert.ToInt32(Console.ReadLine());
+
+            if (guess > randomNum)
+            {
+                Console.WriteLine("Your guess is too high.");
+            }
+            else if (guess < randomNum)
+            {
+                Console.WriteLine("Your guess is too low.");
+            }
+            else
+            {
+                Console.WriteLine("Correct!");
+                isCorrectGuess = true;
+            }
+        }
+        Console.WriteLine("Congratulations, you have won the game");
+
+        Console.ReadKey();
+    }
+}
